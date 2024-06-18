3a50a0714f4593aa2affeb2295242a8e8b62b4e1
Holy Lovenia <holy.lovenia@gmail.com>
2024-06-18 16:57:36 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -103,7 +103,7 @@ export const Hero = () => {
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
-          <h1 className="inline">SEACrowd Data Catalogue</h1>
+          <h1 className="inline">SEACrowd Catalogue</h1>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
