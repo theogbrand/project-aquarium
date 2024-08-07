23a3acade28e7447571cad6f9774cdae387ebf8f
ob1 <brandonong@aisingapore.org>
2024-08-07 19:50:00 +0800

    fix render

--- app/catalogs/[slug]/page.tsx
+++ app/catalogs/[slug]/page.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import Treemap from '@/components/Treemap'
 import "react-d3-treemap/dist/react.d3.treemap.css";
 
+const cityToCountry = {
+    "singapore": "Singapore",
+    "kuala lumpur": "Malaysia",
+    "manila": "Philippines",
+    "jakarta": "Indonesia",
+    "bandar seri begawan": "Brunei",
+    "phnom penh": "Cambodia",
+    "hanoi": "Vietnam",
+    "bangkok": "Thailand",
+    "naypyidaw": "Myanmar",
+    "vientiane": "Laos"
+};
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
@@ -9,7 +21,7 @@ export default function Page({ params }: { params: { slug: string } }) {
             <section className="container grid lg:grid-cols-2 place-items-start py-20 md:py-32 gap-10">
                 <div className="text-start space-y-6">
                     <main className="text-5xl md:text-6xl font-bold">
-                        <h1 className="inline">{params.slug}</h1>
+                        <h1 className="inline">{cityToCountry[params.slug as keyof typeof cityToCountry]}</h1>
                     </main>
 
                     <p className="text-lg text-muted-foreground">
@@ -34,24 +46,9 @@ export default function Page({ params }: { params: { slug: string } }) {
 // }
 
 export async function generateStaticParams() {
+    const cities = Object.keys(cityToCountry);
 
-    const countries = [
-        {
-            markerOffset: -15,
-            name: "Singapore",
-            coordinates: [103.8198, 1.3521]
-        },
-        { markerOffset: -15, name: "Kuala-Lumpur", coordinates: [101.6869, 3.1390] },
-        { markerOffset: 25, name: "Manila", coordinates: [120.9842, 14.5995] },
-        { markerOffset: 25, name: "Jakarta", coordinates: [106.8456, -6.2088] },
-        { markerOffset: 25, name: "Bandar-Seri-Begawan", coordinates: [114.9424, 4.9031] },
-        { markerOffset: 25, name: "Phnom-Penh", coordinates: [104.9282, 11.5564] },
-        { markerOffset: -15, name: "Hanoi", coordinates: [105.8342, 21.0285] },
-        { markerOffset: -15, name: "Bangkok", coordinates: [100.5018, 13.7563] },
-        { markerOffset: 25, name: "Naypyidaw", coordinates: [96.1292, 19.7633] },
-        { markerOffset: -15, name: "Vientiane", coordinates: [102.4955, 19.8563] },
-    ];
-    return countries.map((country) => ({
-        slug: country.name.toLowerCase(),
-    }))
+    return cities.map((city) => ({
+        slug: city.replace(/\s+/g, '-')
+    }));
 }
