a068e4b096dcbade21b4e46c72c0f01527aa251e
ob1 <brandonong@aisingapore.org>
2024-08-12 12:11:26 +0700

    chatbubblewidget

---
+++ components/ChatBubbleWidget.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Chat from "@/components/chatbot-widget/chat";
+import { Button } from "@/components/ui/button";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/chatbot-widget/ui/popover";
+import { MessageCircle } from "lucide-react";
+
+
+export default function ChatBubbleWidget() {
+    return (
+        <>
+            <div className="flex bg-gray-50 min-h-screen items-center justify-center">
+                <Popover>
+                    <PopoverContent align="start" className="w-[440px] mr-4">
+                        <Chat />
+                    </PopoverContent>
+                    <PopoverTrigger asChild className="fixed bottom-4 right-4">
+                        <Button
+                            variant="outline"
+                            className="rounded-full w-16 h-16 aspect-square bg-black hover:bg-gray-700"
+                        >
+                            <MessageCircle size={40} className="text-white" />
+                        </Button>
+                    </PopoverTrigger>
+                </Popover>
+            </div>
+        </>
+    );
+}
