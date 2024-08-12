88553cf15de0195b7f1bbaaf0778605b70c28ac6
ob1 <brandonong@aisingapore.org>
2024-08-12 12:19:04 +0700

    fix imports

--- components/chatbot-widget/ui/toaster.tsx
+++ components/chatbot-widget/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/chatbot-widget/ui/toast"
+import { useToast } from "@/components/chatbot-widget/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
